25fa4ae3521a732d4296f5d49223d2e3c5b5ade6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-01 00:04:12 +0530

    cpu_boost: Disable input boosting if battery saver is active

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -13,6 +13,7 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+#include <linux/battery_saver.h>
 #include <linux/sysfs.h>
 
 #define cpu_boost_attr_rw(_name)		\
@@ -254,7 +255,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 {
 	u64 now;
 
-	if (!input_boost_enabled)
+	if (!input_boost_enabled || is_battery_saver_on())
 		return;
 
 	now = ktime_to_us(ktime_get());
