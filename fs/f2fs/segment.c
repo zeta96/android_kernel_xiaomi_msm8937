527d8de13ae08006b60fe139d0113c5bd7563b27
xiongping1 <xiongping1@xiaomi.com>
2021-03-23 11:42:29 +0800

    f2fs: don't grab superblock freeze for flush/ckpt thread
    MIUI-1791109
    
    There are controlled by f2fs_freeze().
    
    This fixes xfstests/generic/068 which is stuck at
    
     task:f2fs_ckpt-252:3 state:D stack:    0 pid: 5761 ppid:     2 flags:0x00004000
     Call Trace:
      __schedule+0x44c/0x8a0
      schedule+0x4f/0xc0
      percpu_rwsem_wait+0xd8/0x140
      ? percpu_down_write+0xf0/0xf0
      __percpu_down_read+0x56/0x70
      issue_checkpoint_thread+0x12c/0x160 [f2fs]
      ? wait_woken+0x80/0x80
      kthread+0x114/0x150
      ? __checkpoint_and_complete_reqs+0x110/0x110 [f2fs]
      ? kthread_park+0x90/0x90
      ret_from_fork+0x22/0x30
    
    Change-Id: Ie00c5250842632a102e2766c5600ad443ede59ec
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: xiongping1 <xiongping1@xiaomi.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -609,8 +609,6 @@ repeat:
 	if (kthread_should_stop())
 		return 0;
 
-	sb_start_intwrite(sbi->sb);
-
 	if (!llist_empty(&fcc->issue_list)) {
 		struct flush_cmd *cmd, *next;
 		int ret;
@@ -630,8 +628,6 @@ repeat:
 		}
 		fcc->dispatch_list = NULL;
 	}
-
-	sb_end_intwrite(sbi->sb);
 
 	wait_event_interruptible(*q,
 		kthread_should_stop() || !llist_empty(&fcc->issue_list));
