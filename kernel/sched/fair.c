93736098e672897c8dfd0273f9beb98a8d1d39ff
Miaohe Lin <linmiaohe@huawei.com>
2023-06-20 16:07:47 +0800

    sched/fair: Use recent_used_cpu to test p->cpus_ptr
    [ Upstream commit ae2ad293d6be143ad223f5f947cca07bcbe42595 ]
    
    When checking whether a recently used CPU can be a potential idle
    candidate, recent_used_cpu should be used to test p->cpus_ptr as
    p->recent_used_cpu is not equal to recent_used_cpu and candidate
    decision is made based on recent_used_cpu here.
    
    Fixes: 89aafd67f28c ("sched/fair: Use prev instead of new target as recent_used_cpu")
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Phil Auld <pauld@redhat.com>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Link: https://lore.kernel.org/r/20230620080747.359122-1-linmiaohe@huawei.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6877,7 +6877,7 @@ static inline int __select_idle_sibling(struct task_struct *p, int prev,
 	    recent_used_cpu != target &&
 	    cpus_share_cache(recent_used_cpu, target) &&
 	    available_idle_cpu(recent_used_cpu) &&
-	    cpumask_test_cpu(p->recent_used_cpu, &p->cpus_allowed)) {
+	    cpumask_test_cpu(recent_used_cpu, &p->cpus_allowed)) {
 		/*
 		 * Replace recent_used_cpu with prev as it is a potential
 		 * candidate for the next wake:
