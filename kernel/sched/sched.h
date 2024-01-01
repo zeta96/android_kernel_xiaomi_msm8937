ee596d99ace0984944610059342d90b362790a5e
Shrikanth Hegde <sshegde@linux.vnet.ibm.com>
2024-01-01 21:16:24 +0530

    sched/fair: Add READ_ONCE() and use existing helper function to access ->avg_irq
    Use existing helper function cpu_util_irq() instead of open-coding
    access to ->avg_irq.
    
    During review it was noted that ->avg_irq could be updated by a
    different CPU than the one which is trying to access it.
    
    ->avg_irq is updated with WRITE_ONCE(), use READ_ONCE to access it
    in order to avoid any compiler optimizations.
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.vnet.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20240101154624.100981-3-sshegde@linux.vnet.ibm.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3043,7 +3043,7 @@ static inline unsigned long cpu_util(int cpu)
 #ifdef CONFIG_HAVE_SCHED_AVG_IRQ
 static inline unsigned long cpu_util_irq(struct rq *rq)
 {
-	return rq->avg_irq.util_avg;
+	return READ_ONCE(rq->avg_irq.util_avg);
 }
 
 static inline
