ccd58f83b59fe0b8b5c6389477aed1a85e724d80
Peter Zijlstra <peterz@infradead.org>
2024-04-03 09:50:20 +0200

    sched: Allow sched_class::dequeue_task() to fail
    Change the function signature of sched_class::dequeue_task() to return
    a boolean, allowing future patches to 'fail' dequeue.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105028.864630153@infradead.org

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1565,7 +1565,7 @@ static void __dequeue_task_dl(struct rq *rq, struct task_struct *p, int flags)
 	dequeue_pushable_dl_task(rq, p);
 }
 
-static void dequeue_task_dl(struct rq *rq, struct task_struct *p, int flags)
+static bool dequeue_task_dl(struct rq *rq, struct task_struct *p, int flags)
 {
 	update_curr_dl(rq);
 	__dequeue_task_dl(rq, p, flags);
@@ -1586,6 +1586,8 @@ static void dequeue_task_dl(struct rq *rq, struct task_struct *p, int flags)
 	 */
 	if (flags & DEQUEUE_SLEEP)
 		task_non_contending(p);
+
+	return true;
 }
 
 /*
