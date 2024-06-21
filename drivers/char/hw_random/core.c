29fad4b95cabb73a5dc41674529bbecc2c150374
Harald Freudenberger <freude@linux.ibm.com>
2024-06-21 17:02:24 +0200

    hwrng: core - Fix wrong quality calculation at hw rng registration
    When there are rng sources registering at the hwrng core via
    hwrng_register() a struct hwrng is delivered. There is a quality
    field in there which is used to decide which of the registered
    hw rng sources will be used by the hwrng core.
    
    With commit 16bdbae39428 ("hwrng: core - treat default_quality as
    a maximum and default to 1024") there came in a new default of
    1024 in case this field is empty and all the known hw rng sources
    at that time had been reworked to not fill this field and thus
    use the default of 1024.
    
    The code choosing the 'better' hw rng source during registration
    of a new hw rng source has never been adapted to this and thus
    used 0 if the hw rng implementation does not fill the quality field.
    So when two rng sources register, one with 0 (meaning 1024) and
    the other one with 999, the 999 hw rng will be chosen.
    
    As the later invoked function hwrng_init() anyway adjusts the
    quality field of the hw rng source, this adjustment is now done
    during registration of this new hw rng source.
    
    Tested on s390 with two hardware rng sources: crypto cards and
    trng true random generator device driver.
    
    Fixes: 16bdbae39428 ("hwrng: core - treat default_quality as a maximum and default to 1024")
    Reported-by: Christian Rund <Christian.Rund@de.ibm.com>
    Suggested-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/char/hw_random/core.c
+++ drivers/char/hw_random/core.c
@@ -162,7 +162,6 @@ static int hwrng_init(struct hwrng *rng)
 	reinit_completion(&rng->cleanup_done);
 
 skip_init:
-	rng->quality = min_t(u16, min_t(u16, default_quality, 1024), rng->quality ?: 1024);
 	current_quality = rng->quality; /* obsolete */
 
 	return 0;
@@ -533,6 +532,9 @@ int hwrng_register(struct hwrng *rng)
 
 	init_completion(&rng->cleanup_done);
 	complete(&rng->cleanup_done);
+
+	/* Adjust quality field to always have a proper value */
+	rng->quality = min_t(u16, min_t(u16, default_quality, 1024), rng->quality ?: 1024);
 
 	if (!current_rng ||
 	    (!cur_rng_set_by_user && rng->quality > current_rng->quality)) {
