ec5fc71f9189a37b90c3b317fbe7a2875473a4b0
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-07-10 06:58:19 +0700

    [SQUASH] Fix maybe-uninitialized warnings
    - part "treewide: Fix remaining (maybe-)uninitialized warnings" for mi8937 kernel

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -3097,7 +3097,7 @@ static int tcp_clean_rtx_queue(struct sock *sk, u32 prior_fack,
 			       struct tcp_sacktag_state *sack, bool ece_ack)
 {
 	const struct inet_connection_sock *icsk = inet_csk(sk);
-	u64 first_ackt, last_ackt;
+	u64 first_ackt, last_ackt = 0;
 	struct tcp_sock *tp = tcp_sk(sk);
 	u32 prior_sacked = tp->sacked_out;
 	u32 reord = tp->snd_nxt; /* lowest acked un-retx un-sacked seq */
