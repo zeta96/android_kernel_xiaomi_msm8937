8bfffe0e2c83adf40f966f3431c78af3507a6fe9
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-07-13 06:01:19 +0700

    Techpack: audio-legacy: ipc: compile out ipc logging from apr-vm conditionally

--- techpack/audio-legacy/ipc/apr_vm.c
+++ techpack/audio-legacy/ipc/apr_vm.c
@@ -1406,10 +1406,12 @@ static int apr_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
+#ifdef CONFIG_IPC_LOGGING	
 	apr_pkt_ctx = ipc_log_context_create(APR_PKT_IPC_LOG_PAGE_CNT,
 						"apr", 0);
 	if (!apr_pkt_ctx)
 		pr_err("%s: Unable to create ipc log context\n", __func__);
+#endif
 
 	ret = of_property_read_string(pdev->dev.of_node,
 				      "qcom,subsys-name",
