320913d93cfefc2de0275e9463606d77a434b778
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-09-10 14:14:24 +0700

    mm/page_alloc: Increase min_free_kbytes

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -320,7 +320,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 1024;
+int min_free_kbytes = 8192;
 int user_min_free_kbytes = -1;
 int watermark_boost_factor __read_mostly;
 int watermark_scale_factor = 15;
@@ -7932,7 +7932,7 @@ void setup_per_zone_wmarks(void)
  */
 int __meminit init_per_zone_wmark_min(void)
 {
-	unsigned long lowmem_kbytes;
+/*	unsigned long lowmem_kbytes;
 	int new_min_free_kbytes;
 
 	lowmem_kbytes = nr_free_buffer_pages() * (PAGE_SIZE >> 10);
@@ -7947,7 +7947,7 @@ int __meminit init_per_zone_wmark_min(void)
 	} else {
 		pr_warn("min_free_kbytes is not updated to %d because user defined value %d is preferred\n",
 				new_min_free_kbytes, user_min_free_kbytes);
-	}
+	} */
 	setup_per_zone_wmarks();
 	refresh_zone_stat_thresholds();
 	setup_per_zone_lowmem_reserve();
