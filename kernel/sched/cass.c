f3ece3bbd69dbc16d3d8d4466dd1ddc4b40cef3f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-11-02 07:51:58 +0700

    sched/cass: Fix compatibility with WALT
    select_task_rq_fair() has its 5th argument "int sibling_count_hint"
    which CASS lacks. Add it to CASS just to satisfy the compiler and
    the function calls.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -348,8 +348,14 @@ static int cass_select_task_rq(struct task_struct *p, int prev_cpu,
 	return cass_best_cpu(p, prev_cpu, sync, rt);
 }
 
+#ifdef CONFIG_SCHED_WALT
+static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
+				    int sd_flag, int wake_flags,
+				    int sibling_count_hint)
+#else
 static int cass_select_task_rq_fair(struct task_struct *p, int prev_cpu,
 				    int sd_flag, int wake_flags, int sibling_count_hint)
+#endif
 {
 	return cass_select_task_rq(p, prev_cpu, wake_flags, false);
 }
