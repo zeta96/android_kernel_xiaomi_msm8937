8c984b534b57779409dd1c7282682196b2c84ddd
Alexander Winkowski <dereference23@outlook.com>
2024-11-03 21:27:30 +0700

    sched/walt:walt: keep walt task rotation enabled
    - We need to keep it enabled ootb since we have core_ctl nuked.
    
    Suggested-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -97,7 +97,11 @@ static void release_rq_locks_irqrestore(const cpumask_t *cpus,
 __read_mostly unsigned int sysctl_sched_cpu_high_irqload = TICK_NSEC;
 
 unsigned int sysctl_sched_walt_rotate_big_tasks;
+#ifndef CONFIG_SCHED_CORE_CTL
+unsigned int walt_rotation_enabled = 1;
+#else
 unsigned int walt_rotation_enabled;
+#endif
 
 __read_mostly unsigned int sysctl_sched_asym_cap_sibling_freq_match_pct = 100;
 __read_mostly unsigned int sched_ravg_hist_size = 5;
