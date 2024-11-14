5917727ad2b0be32d0c75f070383c3b25fa2cc96
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-11-14 22:29:29 +0700

    sysctl: r/o sched_child_runs_first

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -417,7 +417,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "sched_child_runs_first",
 		.data		= &sysctl_sched_child_runs_first,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= proc_dointvec,
 	},
 #if defined(CONFIG_PREEMPT_TRACER) && defined(CONFIG_PREEMPTIRQ_EVENTS)
