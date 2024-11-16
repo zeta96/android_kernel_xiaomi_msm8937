5568b354d571f3bd671b436ad5c3e74b3cc68ece
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-11-16 08:15:28 +0700

    msm: mdss: guard boost DDR bus with ifdef

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -38,7 +38,9 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/sched.h>
+#ifdef CONFIG_DEVFREQ_BOOST
 #include <linux/devfreq_boost.h>
+#endif
 #include <uapi/linux/sched/types.h>
 #if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
 #include <xiaomi-sdm439/mach.h>
@@ -5341,7 +5343,9 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+#ifdef CONFIG_DEVFREQ_BOOST
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
