49290fddd1676bd042b9d1b5ddfcd36922b98c35
zeta96 <aster.mojave-ter.aster@tutamail.com>
2024-11-27 07:26:49 +0700

    kernel: time: Add guard QUEUED_SPINLOCKS for "Add delay after cpu_relax() in tight loops"
    - This isn't needed if we have queued spinlocks for arm64
    commit a84e7de.

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -974,7 +974,9 @@ static struct timer_base *lock_timer_base(struct timer_list *timer,
 			raw_spin_unlock_irqrestore(&base->lock, *flags);
 		}
 		cpu_relax();
+#ifndef CONFIG_QUEUED_SPINLOCKS
 		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
+#endif
 	}
 }
 
@@ -1434,7 +1436,9 @@ int del_timer_sync(struct timer_list *timer)
 		if (unlikely(ret < 0)) {
 			del_timer_wait_running(timer);
 			cpu_relax();
+#ifndef CONFIG_QUEUED_SPINLOCKS
 			ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
+#endif
 		}
 	} while (ret < 0);
 
