e28ddc545b54cb993c887d0c6139c7d97dc4c3e1
zeta96 <ahmad.zainul857@gmail.com>
2024-11-30 00:14:01 +0000

    Revert "cpufreq: schedutil: Apply protection userspace for hispeed_freq"
    This reverts commit 54fb6f257860e2a1423c1a0bbd18d27fb1670d53.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -997,8 +997,6 @@ static ssize_t hispeed_freq_store(struct gov_attr_set *attr_set,
 	unsigned long hs_util;
 	unsigned long flags;
 
-	return count;
-
 	if (kstrtouint(buf, 10, &val))
 		return -EINVAL;
 
