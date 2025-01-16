6233a94524675a49590fae16c60e21b4604f464f
zeta96 <ahmad.zainul857@gmail.com>
2025-01-16 23:49:28 +0000

    fs: ntfs3: Fix -Wmaybe-uninitialized

--- fs/ntfs3/fsntfs.c
+++ fs/ntfs3/fsntfs.c
@@ -1412,8 +1412,8 @@ int ntfs_write_bh(struct ntfs_sb_info *sbi, struct NTFS_RECORD_HEADER *rhdr,
 	u16 fo = le16_to_cpu(rhdr->fix_off);
 	u16 fn = le16_to_cpu(rhdr->fix_num);
 	u32 idx;
-	__le16 *fixup;
-	__le16 sample;
+	__le16 *fixup = NULL;
+	__le16 sample = 0;
 
 	if ((fo & 1) || fo + fn * sizeof(short) > SECTOR_SIZE || !fn-- ||
 	    fn * SECTOR_SIZE > bytes) {
