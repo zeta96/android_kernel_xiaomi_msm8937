9caf454c33175436de4881e5e045858d8e415f10
zeta96 <ahmad.zainul857@gmail.com>
2025-02-19 02:36:04 +0000

    mm/page_alloc: Increase min_free_kbytes a bit

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -320,7 +320,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 8192;
+int min_free_kbytes = 16384;
 int user_min_free_kbytes = -1;
 #ifdef CONFIG_DISCONTIGMEM
 /*
