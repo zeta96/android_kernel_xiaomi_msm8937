45d1e3b1583c3ff7f3f91b6fa446ef85d23aa289
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    kernel/sys: only spoof bpf for root process
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1287,11 +1287,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	if (current_uid().val == 0 && 
+		(!strncmp(current->comm, "bpfloader", 9) ||
+		!strncmp(current->comm, "netbpfload", 10) ||
+		!strncmp(current->comm, "netd", 4))) {
 		strcpy(tmp.release, "5.4.293");
-		pr_debug("fake uname: %s/%d release=%s\n",
+		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
