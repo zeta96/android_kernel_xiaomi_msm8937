8c24d144b691ae68eadd28ebeadaace6ac85a104
zeta96 <ahmad.zainul857@gmail.com>
2025-04-10 03:14:06 +0000

    Revert "msm: mdss:  Do not boost DDR bus after input timeout"
    This reverts commit 934cd731d70eedae40e2b9110f39b2b9b6de52e5.

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5344,9 +5344,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		break;
 	case MSMFB_ATOMIC_COMMIT:
 #ifdef CONFIG_DEVFREQ_BOOST
-		if (df_boost_within_input(3250)) {
-			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		}
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 #endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
