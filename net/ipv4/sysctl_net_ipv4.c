f8e8b912ade4c38e48fd9b1e109e19137c3fb7d0
zeta96 <ahmad.zainul857@gmail.com>
2025-06-16 20:48:06 +0000

    net-tcp_bbr: v2: Fix PLB in TCP and BBRv2
    - our kernel doesn't have commit "BACKPORT: proc/sysctl: add shared variables for range check"

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -1276,8 +1276,8 @@ static struct ctl_table ipv4_net_table[] = {
 		.maxlen         = sizeof(int),
 		.mode           = 0644,
 		.proc_handler   = proc_dointvec_minmax,
-		.extra1         = SYSCTL_ZERO,
-		.extra2         = SYSCTL_ONE,
+		.extra1         = &zero,
+		.extra2         = &one,
 	},
 	{
 		.procname       = "tcp_plb_idle_rehash_rounds",
