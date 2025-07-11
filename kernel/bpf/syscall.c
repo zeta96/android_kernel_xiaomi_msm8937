f8fea205537e60394005912da1639bd8cacf903f
zeta96 <ahmad.zainul857@gmail.com>
2025-07-11 04:51:56 +0000

    Revert "bpf: avoid dynamic memory allocation for small value buffers"
    This reverts commit cc6f176f9763f782e13d683e076105654d2fdd2e.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -722,7 +722,6 @@ static int map_lookup_elem(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *value, *ptr;
-	char value_onstack[32];
 	u32 value_size;
 	struct fd f;
 	int err;
@@ -765,14 +764,10 @@ static int map_lookup_elem(union bpf_attr *attr)
 	else
 		value_size = map->value_size;
 
-	if (likely(value_size <= sizeof(value_onstack))) {
-		value = value_onstack;
-	} else {
-		err = -ENOMEM;
-		value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
-		if (!value)
-			goto free_key;
-	}
+	err = -ENOMEM;
+	value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
+	if (!value)
+		goto free_key;
 
 	if (bpf_map_is_dev_bound(map)) {
 		err = bpf_map_offload_lookup_elem(map, key, value);
@@ -831,8 +826,7 @@ done:
 	err = 0;
 
 free_value:
-	if (unlikely(value != value_onstack))
-		kfree(value);
+	kfree(value);
 free_key:
 	kfree(key);
 err_put:
@@ -860,7 +854,6 @@ static int map_update_elem(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *value;
-	char value_onstack[16];
 	u32 value_size;
 	struct fd f;
 	int err;
@@ -898,14 +891,10 @@ static int map_update_elem(union bpf_attr *attr)
 	else
 		value_size = map->value_size;
 
-	if (likely(value_size <= sizeof(value_onstack))) {
-		value = value_onstack;
-	} else {
-		err = -ENOMEM;
-		value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
-		if (!value)
-			goto free_key;
-	}
+	err = -ENOMEM;
+	value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
+	if (!value)
+		goto free_key;
 
 	err = -EFAULT;
 	if (copy_from_user(value, uvalue, value_size) != 0)
@@ -956,8 +945,7 @@ static int map_update_elem(union bpf_attr *attr)
 	maybe_wait_bpf_programs(map);
 out:
 free_value:
-	if (unlikely(value != value_onstack))
-		kfree(value);
+	kfree(value);
 free_key:
 	kfree(key);
 err_put:
@@ -1025,7 +1013,6 @@ static int map_get_next_key(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *next_key;
-	char next_key_onstack[48];
 	struct fd f;
 	int err;
 
@@ -1052,14 +1039,10 @@ static int map_get_next_key(union bpf_attr *attr)
 		key = NULL;
 	}
 
-	if (likely(map->key_size <= sizeof(next_key_onstack))) {
-		next_key = next_key_onstack;
-	} else {
-		err = -ENOMEM;
-		next_key = kmalloc(map->key_size, GFP_USER);
-		if (!next_key)
-			goto free_key;
-	}
+	err = -ENOMEM;
+	next_key = kmalloc(map->key_size, GFP_USER);
+	if (!next_key)
+		goto free_key;
 
 	if (bpf_map_is_dev_bound(map)) {
 		err = bpf_map_offload_get_next_key(map, key, next_key);
@@ -1080,8 +1063,7 @@ out:
 	err = 0;
 
 free_next_key:
-	if (unlikely(next_key != next_key_onstack))
-		kfree(next_key);
+	kfree(next_key);
 free_key:
 	kfree(key);
 err_put:
