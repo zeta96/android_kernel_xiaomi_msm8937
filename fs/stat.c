e3579bf953d2e4606e3658333dd2a7d0e4e5e598
zeta96 <ahmad.zainul857@gmail.com>
2025-07-22 08:28:14 +0000

    KernelSU: fix build by adding dependency on CONFIG_KSU_KPROBES_HOOK

--- fs/stat.c
+++ fs/stat.c
@@ -353,7 +353,7 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 	return cp_new_stat(&stat, statbuf);
 }
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
 extern __attribute__((hot)) int ksu_handle_stat(int *dfd,
 			                    const char __user **filename_user, int *flags);
 #endif
@@ -365,7 +365,7 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
@@ -519,7 +519,7 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#ifdef CONFIG_KSU // 32-bit su
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK) // 32-bit su
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
