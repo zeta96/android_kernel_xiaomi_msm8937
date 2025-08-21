ce56619c2b3a9e22de453df74ef20a4f77111a72
zeta96 <aster.mojave-ter.aster@tutamail.com>
2025-08-21 19:54:11 +0700

    Update mux.c

--- drivers/usb/typec/mux.c
+++ drivers/usb/typec/mux.c
@@ -189,7 +189,6 @@ static int mux_fwnode_match(struct device *dev, const void *fwnode)
 static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 {
 	const struct typec_altmode_desc *desc = data;
-<<<<<<< HEAD
 	struct device *dev;
 	bool match;
 	int nval;
@@ -202,19 +201,6 @@ static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 					con->endpoint[ep], name_match);
 
 		return dev ? to_typec_switch(dev) : ERR_PTR(-EPROBE_DEFER);
-=======
-	struct typec_mux *mux;
-	int nval;
-	bool match;
-	u16 *val;
-	int i;
-
-	if (!con->fwnode) {
-		list_for_each_entry(mux, &mux_list, entry)
-			if (!strcmp(con->endpoint[ep], dev_name(mux->dev)))
-				return mux;
-		return ERR_PTR(-EPROBE_DEFER);
->>>>>>> 6f1d442369b6d40c55f035d443d8187f610385b9
 	}
 
 	/*
@@ -234,11 +220,7 @@ static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 	}
 
 	/* Alternate Mode muxes */
-<<<<<<< HEAD
 	nval = fwnode_property_count_u16(con->fwnode, "svid");
-=======
-	nval = fwnode_property_read_u16_array(con->fwnode, "svid", NULL, 0);
->>>>>>> 6f1d442369b6d40c55f035d443d8187f610385b9
 	if (nval <= 0)
 		return NULL;
 
@@ -246,17 +228,10 @@ static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 	if (!val)
 		return ERR_PTR(-ENOMEM);
 
-<<<<<<< HEAD
 	ret = fwnode_property_read_u16_array(con->fwnode, "svid", val, nval);
 	if (ret < 0) {
 		kfree(val);
 		return ERR_PTR(ret);
-=======
-	nval = fwnode_property_read_u16_array(con->fwnode, "svid", val, nval);
-	if (nval < 0) {
-		kfree(val);
-		return ERR_PTR(nval);
->>>>>>> 6f1d442369b6d40c55f035d443d8187f610385b9
 	}
 
 	for (i = 0; i < nval; i++) {
@@ -270,18 +245,10 @@ static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 	return NULL;
 
 find_mux:
-<<<<<<< HEAD
 	dev = class_find_device(&typec_mux_class, NULL, con->fwnode,
 				mux_fwnode_match);
 
 	return dev ? to_typec_mux(dev) : ERR_PTR(-EPROBE_DEFER);
-=======
-	list_for_each_entry(mux, &mux_list, entry)
-		if (dev_fwnode(mux->dev) == con->fwnode)
-			return mux;
-
-	return match ? ERR_PTR(-EPROBE_DEFER) : NULL;
->>>>>>> 6f1d442369b6d40c55f035d443d8187f610385b9
 }
 
 /**
