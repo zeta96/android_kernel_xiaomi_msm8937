5357a602411b12ed576402414c526e3a5fe49c47
Tim Zimmermann <tim@linux4.de>
2025-09-04 22:20:47 +0300

    syscall: Fake uname to 5.10.239 for bpfloader/netbpfload/netd

--- kernel/sys.c
+++ kernel/sys.c
@@ -1250,7 +1250,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
-		strcpy(tmp.release, "5.10.199");
+		strcpy(tmp.release, "5.10.239");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
