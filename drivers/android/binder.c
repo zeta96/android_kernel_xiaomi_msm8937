e6a83e11a3e8a215b25b7a59bbe35e4f7f16349f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-09-21 01:41:25 +0600

    binder: Fix potential NULL pointer dereference in binder_request_freeze_notification()
    In binder_request_freeze_notification(), a local variable `freeze` is
    allocated with kmem_cache_zalloc(). If binder_get_ref_olocked() fails, it
    returns NULL, and so `ref` is NULL. The NULL condition erroneously calls
    kmem_cache_free(..., ref->freeze) on those error paths, which dereferences
    `ref` and can trigger a NULL pointer dereference.
    
    Fix this by correctly freeing the locally allocated `freeze` on all error
    paths.
    
    Fixes: 7287309df17ee2 ("binder: Reserve caches for small, high-frequency memory allocations")
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3989,7 +3989,7 @@ binder_request_freeze_notification(struct binder_proc *proc,
 		binder_user_error("%d:%d BC_REQUEST_FREEZE_NOTIFICATION invalid ref %d\n",
 				  proc->pid, thread->pid, handle_cookie->handle);
 		binder_proc_unlock(proc);
-		kmem_cache_free(binder_ref_freeze_pool, ref->freeze);
+		kmem_cache_free(binder_ref_freeze_pool, freeze);
 		return -EINVAL;
 	}
 
@@ -3999,7 +3999,7 @@ binder_request_freeze_notification(struct binder_proc *proc,
 				  proc->pid, thread->pid);
 		binder_node_unlock(ref->node);
 		binder_proc_unlock(proc);
-		kmem_cache_free(binder_ref_freeze_pool, ref->freeze);
+		kmem_cache_free(binder_ref_freeze_pool, freeze);
 		return -EINVAL;
 	}
 
