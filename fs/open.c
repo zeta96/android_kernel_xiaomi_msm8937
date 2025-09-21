f35c9ff66fa12a552e44540012253647ecf5ca34
zeta96 <ahmad.zainul857@gmail.com>
2025-09-21 07:28:47 +0000

    KernelSU: added ksun compatible min scope syscall hooks v1.5 by @backslashxx & @pershoot
    Implementing guide: backslashxx/KernelSU#5

--- fs/open.c
+++ fs/open.c
@@ -451,8 +451,17 @@ out:
 	return res;
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+__attribute__((hot))
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+		               int *mode, int *flags);
+#endif
+
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 	return do_faccessat(dfd, filename, mode);
 }
 
