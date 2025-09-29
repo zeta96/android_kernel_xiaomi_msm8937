6a04de367c41f26aa16e81ebfad78dfae3cfd7f5
zeta96 <aster.mojave-ter.aster@tutamail.com>
2025-09-29 17:31:45 +0700

    fix "sched/fair: Allow changing cgroup of new forked task"
    - our kernel still use state, not __state

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -13385,7 +13385,7 @@ static void task_change_group_fair(struct task_struct *p)
 	 * We couldn't detach or attach a forked task which
 	 * hasn't been woken up by wake_up_new_task().
 	 */
-	if (READ_ONCE(p->__state) == TASK_NEW)
+	if (READ_ONCE(p->state) == TASK_NEW)
 		return;
 
 	detach_task_cfs_rq(p);
