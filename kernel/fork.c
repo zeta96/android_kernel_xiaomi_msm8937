d12918740d6f0eb91341a368c0998f26d75d6e01
zeta96 <ahmad.zainul857@gmail.com>
2025-10-02 08:47:52 +0000

    kernel: Reduce Boost DDR bus for a short amount of time when zygote forks
    - light boost

--- kernel/fork.c
+++ kernel/fork.c
@@ -2334,11 +2334,11 @@ long _do_fork(unsigned long clone_flags,
 
 #ifdef CONFIG_DEVFREQ_BOOST
         /*
-         * Boost to the max for 50 ms when userspace launches an app. Only
+         * Boost to the max for 32 ms when userspace launches an app. Only
          * if within 1.5s input timeout.
          */
 	if (task_is_zygote(current) && df_boost_within_input(1500)) {
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
 	}
 #endif
 
