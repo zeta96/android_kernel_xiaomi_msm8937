1ba8b8924ebe4dafc6ca6e3891bae3572d5dc59a
zeta96 <ahmad.zainul857@gmail.com>
2026-01-05 05:15:58 +0700

    sched/tune: fix stune assist

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -767,33 +767,47 @@ static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
 }
 #endif
 
-#ifdef CONFIG_STUNE_ASSIST
 static struct cftype files[] = {
 #ifdef CONFIG_SCHED_WALT
 	{
 		.name = "sched_boost_no_override",
 		.read_u64 = sched_boost_override_read,
+#ifdef CONFIG_STUNE_ASSIST
 		.write_u64 = sched_boost_override_write_wrapper,
+#else
+		.write_u64 = sched_boost_override_write,
+#endif
 	},
 	{
 		.name = "colocate",
 		.read_u64 = sched_colocate_read,
+#ifdef CONFIG_STUNE_ASSIST
 		.write_u64 = sched_colocate_write_wrapper,
+#else
+		.write_u64 = sched_colocate_write,
+#endif
 	},
 #endif
 	{
 		.name = "boost",
 		.read_s64 = boost_read,
+#ifdef CONFIG_STUNE_ASSIST
 		.write_s64 = boost_write_wrapper,
+#else
+		.write_s64 = boost_write,
+#endif
 	},
 	{
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
+#ifdef CONFIG_STUNE_ASSIST
 		.write_u64 = prefer_idle_write_wrapper,
+#else
+		.write_u64 = prefer_idle_write,
+#endif
 	},
 	{ }	/* terminate */
 };
-#endif
 
 static void
 schedtune_boostgroup_init(struct schedtune *st, int idx)
