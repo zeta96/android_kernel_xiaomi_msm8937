3577df5656a5ee2724ce68ccb70e650427aad16c
zeta96 <ahmad.zainul857@gmail.com>
2026-01-06 14:31:00 +0700

    Add dummy schedtune functions for Android userspace
    Implemented dummy schedtune functions for Android compatibility.

--- kernel/sched/tune_dummy.c
+++ kernel/sched/tune_dummy.c
@@ -1,16 +1,49 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/cgroup.h>
 
-static int schedtune_css_online(struct cgroup_subsys_state *css)
+/*
+ * Dummy schedtune for Android userspace compatibility.
+ * Does NOT affect scheduler behavior.
+ */
+
+static s64 boost_read(struct cgroup_subsys_state *css,
+                      struct cftype *cft)
 {
     return 0;
 }
 
-static void schedtune_css_offline(struct cgroup_subsys_state *css)
+static int boost_write(struct cgroup_subsys_state *css,
+                       struct cftype *cft, s64 val)
 {
+    return 0;
 }
 
+static u64 prefer_idle_read(struct cgroup_subsys_state *css,
+                            struct cftype *cft)
+{
+    return 0;
+}
+
+static int prefer_idle_write(struct cgroup_subsys_state *css,
+                             struct cftype *cft, u64 val)
+{
+    return 0;
+}
+
+static struct cftype schedtune_files[] = {
+    {
+        .name       = "boost",
+        .read_s64   = boost_read,
+        .write_s64  = boost_write,
+    },
+    {
+        .name       = "prefer_idle",
+        .read_u64   = prefer_idle_read,
+        .write_u64  = prefer_idle_write,
+    },
+    { }
+};
+
 struct cgroup_subsys schedtune_cgrp_subsys = {
-    .css_online  = schedtune_css_online,
-    .css_offline = schedtune_css_offline,
+    .legacy_cftypes = schedtune_files,
 };
