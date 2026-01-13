bae58331a3bd24e3d820064d90bb6e2fd1bf869b
zeta96 <ahmad.zainul857@gmail.com>
2026-01-13 22:46:32 +0000

    cpufreq: schedutil: tweak default up/down rate limits

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -806,8 +806,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = 500;
-	tunables->down_rate_limit_us = 1000;
+	tunables->up_rate_limit_us = 1000;
+	tunables->down_rate_limit_us = 4000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
